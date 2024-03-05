269f0bca69d338d7693a311d0670652e5b1e434b
dicenull <dicenull.dev@gmail.com>
2024-03-06 00:22:38 +0900

    ✨ 局と場の表示を追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+final roundProvider = StateProvider<int>((ref) => 0);
+
+final honbaProvider = StateProvider<int>((ref) => 0);
+
+final honbaNameProvider = Provider<String>((ref) {
+  final honba = ref.watch(honbaProvider);
+  return '$honba 本場';
+});
+
+final roundNameProvider = Provider<String>((ref) {
+  final round = ref.watch(roundProvider);
+  return switch (round) {
+    0 => '東一局',
+    1 => '東二局',
+    2 => '東三局',
+    3 => '東四局',
+    4 => '南一局',
+    5 => '南二局',
+    6 => '南三局',
+    7 => '南四局',
+    8 => '西一局',
+    9 => '西二局',
+    10 => '西三局',
+    11 => '西四局',
+    12 => '北一局',
+    13 => '北二局',
+    14 => '北三局',
+    15 => '北四局',
+    _ => throw UnimplementedError(),
+  };
+});
 
 void main() {
-  runApp(const App());
+  runApp(const ProviderScope(child: App()));
 }
 
 class App extends StatelessWidget {
@@ -30,49 +63,84 @@ class App extends StatelessWidget {
   }
 }
 
-class _Compass extends StatelessWidget {
+class _Compass extends ConsumerWidget {
   const _Compass(this.centerPadding);
 
   final double centerPadding;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final round = ref.watch(roundProvider);
+
     return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          const Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              SizedBox.shrink(),
-              _WindText(Wind.west),
-              SizedBox.shrink(),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              const _WindText(Wind.north),
-              SizedBox(
-                width: centerPadding,
-                height: centerPadding,
-              ),
-              const _WindText(Wind.south),
-            ],
-          ),
-          const Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              SizedBox.shrink(),
-              _WindText(Wind.east),
-              SizedBox.shrink(),
-            ],
-          ),
-        ],
+      child: AnimatedRotation(
+        turns: -round / 4,
+        duration: const Duration(milliseconds: 500),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                SizedBox.shrink(),
+                _WindText(Wind.west),
+                SizedBox.shrink(),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                const _WindText(Wind.north),
+                SizedBox(
+                  width: centerPadding,
+                  height: centerPadding,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {
+                          ref
+                              .read(roundProvider.notifier)
+                              .update((state) => state + 1);
+                          ref.read(honbaProvider.notifier).update((state) => 0);
+                        },
+                        child: Text(
+                          ref.watch(roundNameProvider),
+                          style: const TextStyle(fontSize: 40),
+                        ),
+                      ),
+                      const SizedBox(height: 16),
+                      FilledButton(
+                        onPressed: () {
+                          ref
+                              .read(honbaProvider.notifier)
+                              .update((state) => state + 1);
+                        },
+                        child: Text(
+                          ref.watch(honbaNameProvider),
+                          style: const TextStyle(fontSize: 40),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const _WindText(Wind.south),
+              ],
+            ),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                SizedBox.shrink(),
+                _WindText(Wind.east),
+                SizedBox.shrink(),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
