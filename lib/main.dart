57eec1c60c3e1e2a8cb7ef8526eac334ec100bb6
dicenull <dicenull.dev@gmail.com>
2024-03-06 13:06:53 +0900

    ✨ サイコロWidget作成

--- lib/main.dart
+++ lib/main.dart
@@ -100,6 +100,15 @@ class _Compass extends ConsumerWidget {
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
+                      // Wrap(
+                      //   children: List.generate(
+                      //     6,
+                      //     (index) => Padding(
+                      //       padding: const EdgeInsets.all(8.0),
+                      //       child: _Die(n: index + 1, size: 40),
+                      //     ),
+                      //   ),
+                      // ),
                       ElevatedButton(
                         onPressed: () {
                           ref
@@ -142,6 +151,77 @@ class _Compass extends ConsumerWidget {
           ],
         ),
       ),
+    );
+  }
+}
+
+class _Die extends StatelessWidget {
+  const _Die({required this.n, required this.size});
+
+  final int n;
+  final double size;
+
+  @override
+  Widget build(BuildContext context) {
+    final dSize = size * .2;
+
+    const none = SizedBox.shrink();
+
+    return Container(
+      color: Colors.white,
+      height: size,
+      width: size,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              if (n == 4 || n == 5 || n == 6) _Dot(dSize) else none,
+              none,
+              if (n != 1) _Dot(dSize) else none,
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              if (n == 6) _Dot(dSize) else none,
+              if (n == 1 || n == 3 || n == 5)
+                _Dot(dSize, isRed: n == 1)
+              else
+                none,
+              if (n == 6) _Dot(dSize) else none,
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              if (n != 1) _Dot(dSize) else none,
+              none,
+              if (n == 4 || n == 5 || n == 6) _Dot(dSize) else none,
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _Dot extends StatelessWidget {
+  const _Dot(this.size, {this.isRed = false});
+
+  final double size;
+  final bool isRed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        color: isRed ? Colors.red : Colors.black,
+      ),
+      width: size,
+      height: size,
     );
   }
 }
